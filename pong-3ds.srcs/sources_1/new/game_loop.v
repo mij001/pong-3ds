2d6ab0522306ca34ef0c61353ecc13bb9303451e
Inomal <no@yolo.lol>
2024-12-21 03:18:50 +0530

    bounce fully working

--- pong-3ds.srcs/sources_1/new/game_loop.v
+++ pong-3ds.srcs/sources_1/new/game_loop.v
@@ -91,7 +91,7 @@ module game_loop  #(
     
     // TODO: change with paddle position later
     assign left_hit = (ball_left <= WALL_WIDTH);
-    assign right_hit = (ball_right >= D_WIDTH+WALL_WIDTH);
+    assign right_hit = (ball_right >= D_WIDTH-WALL_WIDTH);
     //////////////////////////////////////// 
     
     reg signed[$clog2(D_WIDTH)+1:0] x_vel_ball;
@@ -100,8 +100,8 @@ module game_loop  #(
     reg signed[$clog2(D_HEIGHT)+1:0] y_vel_ball_next;
 
 //  localparam BARRIER_SIZE = WALL_WIDTH + BALL_RAD + 2;
-    localparam BALL_VEL_POS = 2;//BALL_VEL_ABS;
-    localparam BALL_VEL_NEG = -2;//-1*BALL_VEL_ABS;
+    localparam BALL_VEL_POS = BALL_VEL_ABS;
+    localparam BALL_VEL_NEG = -1*BALL_VEL_ABS;
     
     always @ (posedge clk)
     begin
@@ -135,79 +135,4 @@ module game_loop  #(
             end
         end
     end
-
-//    always @ (posedge clk)
-//    begin
-//        if (rst) begin
-
-            
-//            x_vel_ball <= BALL_VEL_POS;
-//            y_vel_ball <= BALL_VEL_POS;
-            
-//            paddle_y <= D_HEIGHT/2;
-//        end else begin
-//            ball_x <= ball_x_next;
-//            ball_y <= ball_y_next;
-            
-//            x_vel_ball <= x_vel_ball_next;
-//            y_vel_ball <= y_vel_ball_next;
-            
-//            paddle_x <= paddle_x_next;
-//            paddle_y <= paddle_y_next;
-//        end
-//    end
-//    ///////////////
-//    always @ * begin
-//        // frame begining indicatior
-//        // calculations starts at the end of the previous
-//        // v-sync to give time to comb circuits to settle 
-        
-//        ball_x_next = ball_x;
-//        ball_y_next = ball_y;
-        
-//        x_vel_ball_next = x_vel_ball;
-//        y_vel_ball_next = y_vel_ball;
-        
-//        paddle_x_next = PADDLE_WIDTH/2;
-//        paddle_y_next = paddle_y;
-        
-//        if (frame_start) begin
-//            /////////////////// this can now be considered as a game loop //////////////////
-//            // reverse ball direction fi hits wall
-//            // horizontal
-//            if (ball_x <= BARRIER_SIZE) begin 
-//                x_vel_ball_next = BALL_VEL_POS;
-//                //ball_x_next = BARRIER_SIZE;
-//            end  else if (ball_x >= D_WIDTH-BARRIER_SIZE) begin 
-//                x_vel_ball_next = BALL_VEL_NEG;
-//                //ball_x_next = BARRIER_SIZE;
-//            end  else begin
-//                x_vel_ball_next = x_vel_ball;
-//            end
-//            // vertical
-//            if (ball_y <= BARRIER_SIZE) begin 
-//                y_vel_ball_next = BALL_VEL_POS;
-//                //ball_y_next = BARRIER_SIZE;
-//            end  else if (ball_y >= D_HEIGHT-BARRIER_SIZE) begin 
-//                y_vel_ball_next = BALL_VEL_NEG;
-//                //ball_y_next = BARRIER_SIZE;
-//            end  else begin
-//                y_vel_ball_next = y_vel_ball;
-//            end
-            
-//            // update position of the ball
-//            ball_x_next = ball_x+x_vel_ball;
-//            ball_y_next = ball_x+y_vel_ball;
-//            ////////////////////////////////////////////////////////////////////////////////
-//        end else begin
-//            ball_x_next = ball_x;
-//            ball_y_next = ball_y;
-            
-//            x_vel_ball_next = x_vel_ball;
-//            y_vel_ball_next = y_vel_ball;
-            
-//            paddle_x_next = PADDLE_WIDTH/2;
-//            paddle_y_next = paddle_y;
-//        end
-//    end
 endmodule
